1609d5830c291282a757280415a3d2bcbd27fd45
Sky388-bit <skydefender388@gmail.com>
2026-03-08 11:28:16 -0400

    End of part 1

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
 
-def main():
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
